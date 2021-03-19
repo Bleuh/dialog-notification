ea6e5d28b435a8c61d60861779b7d7516b6d120b
Bastien Douib <bastiendouib@gmail.com>
2021-03-19 02:16:50 +0100

    Init project with function / fcm

---
+++ src/preload.ts
@@ -0,0 +1,21 @@
+
+import * as Electron from "electron";
+const ElectronAPI: Record<string, unknown> =
+{
+    ipcRenderer:
+    {
+        send: function (channel: string, ...args: unknown[]): void
+        {
+            Electron.ipcRenderer.send(channel, ...args);
+        },
+        on: function (channel: string, callback: (event: Electron.IpcRendererEvent, ...args: unknown[]) => void): Electron.IpcRenderer
+        {
+            return Electron.ipcRenderer.on(channel, callback);
+        },
+        removeAllListeners: function (channel: string): Electron.IpcRenderer
+        {
+            return Electron.ipcRenderer.removeAllListeners(channel);
+        }
+    }
+};
+Electron.contextBridge.exposeInMainWorld("Electron", ElectronAPI);
